9e0890e25d24bbdc0b9f9420009f28bc24ee17f9
oluseunakintayo <oluseun.akintayo@gmail.com>
2024-07-17 19:55:09 +0100

    Update ProtectedRoute.tsx

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -3,8 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 const token: string | null = sessionStorage.getItem('command');
 
 export default function ProtectedRoute() {
-  if(!token) {
-    return <Navigate to="/auth/login" />
-  }
+  if(!token) return <Navigate to="/auth/login" />
   return <Outlet />
 }
